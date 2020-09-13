95b7f3841623acf242e32b25e508c035662d7595
Varun Kumaar <teralad@gmail.com>
2020-09-13 16:19:08 +0530

    other: Adding subscription component

---
+++ src/app/components/subscription.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Switch } from "antd";
+import Toast from "react-bootstrap/Toast";
+import PropTypes from "prop-types";
+
+let Subscription = (props) => {
+  const [title, setTitle] = useState(props.msg.title);
+  const [body, setBody] = useState(props.msg.body);
+  const [toggle, setToggle] = useState(false);
+  const [show, setShow] = useState(false);
+
+  function toggleChange(changed) {
+    setToggle(changed);
+    setShow(true);
+  }
+
+  return (
+    <div>
+      <div className="sub">
+        <div className="sub-titlebar">
+          <div className="sub-title">{title}</div>
+          <div className="sub-toggle">
+            <Switch onClick={toggleChange} />
+          </div>
+        </div>
+        <div className="sub-body">{body}</div>
+      </div>
+      <div className="footer">
+        <Toast onClose={() => setShow(false)} show={show} delay={1000} autohide>
+          <Toast.Body>Your changes have been saved!</Toast.Body>
+        </Toast>
+      </div>
+    </div>
+  );
+};
+
+Subscription.propTypes = {
+  msg: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+  }),
+};
+
+export default Subscription;
