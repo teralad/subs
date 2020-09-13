be596bff70c4789b255e5ed75b1f2d205a58d2ec
Varun Kumaar <teralad@gmail.com>
2020-09-13 16:17:43 +0530

    other: initializing base files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { API } from "./Api";
+import Subscription from "../src/app/components/subscription";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    staticsubscriptions: [],
+    subscriptions: [],
+    err: false,
+  };
+  componentDidMount() {
+    // this.getSubscriptions();
+    this.getStaticSubscriptions();
+  }
+  componentDidCatch(error, info) {
+    return <h1>Something went wrong... Please try again</h1>;
+  }
+
+  getStaticSubscriptions = () => {
+    API.getStaticSubscriptions().then((data) => {
+      this.setState({ staticsubscriptions: data.data, subscriptions: data.data });
+    });
+  };
+
+  getSubscriptions = () => {
+    API.getSubscriptions()
+      .then((resp) => resp.json())
+      .then((data) => {
+        this.setState({ subscriptions: data, err: false });
+      })
+      .catch((err) => this.setState({ err: true }));
+  };
+  render() {
+    const { err, subscriptions } = this.state;
+
+    if (err) {
+      return <div>Somthing went wrong. Please try again</div>;
+    }
+
+    return (
+      <div className="app">
+        <div className="app-header">
+          <header>SUBSCRIPTIONS</header>
+        </div>
+        <div className="subscriptions">
+          {subscriptions.length > 0 &&
+            subscriptions.slice(0, 20).map((subscription) => <Subscription key={subscription.id} msg={subscription} />)}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
